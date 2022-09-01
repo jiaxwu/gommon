9d06e0f420c3d869cb092f3e1b7647455eb18f69
xhsf <827032783@qq.com>
2022-09-01 22:54:38 +0800

    math更新

--- math/math.go
+++ math/math.go
@@ -2,6 +2,7 @@ package math
 
 import (
 	"math"
+	"math/bits"
 
 	"golang.org/x/exp/constraints"
 )
@@ -42,6 +43,31 @@ func Abs[T constraints.Float | constraints.Signed](a T) T {
 }
 
 // Log2
+// 修改更加高效算法
 func Log2[T constraints.Integer | constraints.Float](a T) T {
 	return T(math.Log2(float64(a)))
+}
+
+// IsPowOf2 check if a value is a power of two
+// Determine whether some value is a power of two, where zero is
+// not considered a power of two.
+func IsPowOf2[T constraints.Unsigned](n T) bool {
+	return n != 0 && ((n & (n - 1)) == 0)
+}
+
+// RoundUpPowOf2 round up to nearest power of two
+func RoundUpPowOf2[T constraints.Unsigned](n T) T {
+	return 1 << FindLastBitSet(n-1)
+}
+
+// RoundDownPowOf2 round down to nearest power of two
+func RoundDownPowOf2[T constraints.Unsigned](n T) T {
+	return 1 << (FindLastBitSet(n) - 1)
+}
+
+// FindLastBitSet find last (most-significant) bit set
+// This is defined the same way as ffs.
+// Note FindLastBitSet(0) = 0, FindLastBitSet(1) = 1, FindLastBitSet(0x80000000) = 32.
+func FindLastBitSet[T constraints.Unsigned](x T) int {
+	return bits.Len64(uint64(x))
 }
--- math/math_test.go
+++ math/math_test.go
@@ -1,6 +1,9 @@
 package math
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestSplit(t *testing.T) {
 	tests := []struct {
@@ -49,5 +52,201 @@ func TestSplit(t *testing.T) {
 				t.Errorf("Split() last = %v, expected %v", last, tt.last)
 			}
 		})
+	}
+}
+
+func TestIsPowOf2(t *testing.T) {
+	tests := []struct {
+		name string
+		x    uint64
+		is   bool
+	}{
+		{
+			name: "0",
+			x:    0,
+			is:   false,
+		},
+		{
+			name: "1",
+			x:    1,
+			is:   true,
+		},
+		{
+			name: "2",
+			x:    2,
+			is:   true,
+		},
+		{
+			name: "4",
+			x:    4,
+			is:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := IsPowOf2(tt.x)
+			if is != tt.is {
+				t.Errorf("IsPowOf2() is = %v, expected %v", is, tt.is)
+			}
+		})
+	}
+}
+
+func TestRoundUpPowOf2(t *testing.T) {
+	tests := []struct {
+		name string
+		x    uint64
+		up   uint64
+	}{
+		{
+			name: "0",
+			x:    0,
+			up:   0,
+		},
+		{
+			name: "1",
+			x:    1,
+			up:   1,
+		},
+		{
+			name: "2",
+			x:    3,
+			up:   4,
+		},
+		{
+			name: "4",
+			x:    10,
+			up:   16,
+		},
+		{
+			name: "math.MaxUint64",
+			x:    math.MaxUint64,
+			up:   0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			up := RoundUpPowOf2(tt.x)
+			if up != tt.up {
+				t.Errorf("RoundUpPowOf2() up = %v, expected %v", up, tt.up)
+			}
+		})
+	}
+}
+
+func TestRoundDownPowOf2(t *testing.T) {
+	tests := []struct {
+		name string
+		x    uint64
+		down uint64
+	}{
+		{
+			name: "1",
+			x:    1,
+			down: 1,
+		},
+		{
+			name: "2",
+			x:    3,
+			down: 2,
+		},
+		{
+			name: "4",
+			x:    10,
+			down: 8,
+		},
+		{
+			name: "math.MaxUint64",
+			x:    math.MaxUint64,
+			down: 1 << 63,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			down := RoundDownPowOf2(tt.x)
+			if down != tt.down {
+				t.Errorf("RoundDownPowOf2() down = %v, expected %v", down, tt.down)
+			}
+		})
+	}
+}
+
+func TestFindLastBitSet(t *testing.T) {
+	type entry = struct {
+		nlz, ntz, pop int
+	}
+
+	// tab contains results for all uint8 values
+	var tab [256]entry
+
+	tab[0] = entry{8, 8, 0}
+	for i := 1; i < len(tab); i++ {
+		// nlz
+		x := i // x != 0
+		n := 0
+		for x&0x80 == 0 {
+			n++
+			x <<= 1
+		}
+		tab[i].nlz = n
+
+		// ntz
+		x = i // x != 0
+		n = 0
+		for x&1 == 0 {
+			n++
+			x >>= 1
+		}
+		tab[i].ntz = n
+
+		// pop
+		x = i // x != 0
+		n = 0
+		for x != 0 {
+			n += int(x & 1)
+			x >>= 1
+		}
+		tab[i].pop = n
+	}
+	for i := 0; i < 256; i++ {
+		len := 8 - tab[i].nlz
+		for k := 0; k < 64-8; k++ {
+			x := uint64(i) << uint(k)
+			want := 0
+			if x != 0 {
+				want = len + k
+			}
+			if x <= 1<<8-1 {
+				got := FindLastBitSet(uint8(x))
+				if got != want {
+					t.Fatalf("Len8(%#02x) == %d; want %d", x, got, want)
+				}
+			}
+
+			if x <= 1<<16-1 {
+				got := FindLastBitSet(uint16(x))
+				if got != want {
+					t.Fatalf("Len16(%#04x) == %d; want %d", x, got, want)
+				}
+			}
+
+			if x <= 1<<32-1 {
+				got := FindLastBitSet(uint32(x))
+				if got != want {
+					t.Fatalf("Len32(%#08x) == %d; want %d", x, got, want)
+				}
+			}
+
+			if x <= 1<<64-1 {
+				got := FindLastBitSet(uint64(x))
+				if got != want {
+					t.Fatalf("Len64(%#016x) == %d; want %d", x, got, want)
+				}
+				got = FindLastBitSet(uint(x))
+				if got != want {
+					t.Fatalf("Len(%#016x) == %d; want %d", x, got, want)
+				}
+			}
+		}
 	}
 }
