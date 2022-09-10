6068d52ef501e3ce556da2f80df1703baef72015
xhsf <827032783@qq.com>
2022-09-10 17:09:30 +0800

    count min计数器

--- counter/cm/cm.go
+++ counter/cm/cm.go
@@ -8,25 +8,27 @@ import (
 
 const (
 	// 一个计数需要多少位表示
-	countBits = 4
+	countBits uint64 = 4
 	// 一个计数哈希到多少个槽
-	counterDepth = 4
-	// 采样因子，也就是平均每10次计数reset()一次
-	sampleFactor = 10
+	counterDepth uint64 = 4
+	// 计数掩码
+	countMask uint64 = countBits*counterDepth - 1
+	// 采样因子，也就是平均多少次计数Reset()一次
+	sampleFactor uint64 = 8
 )
 
 // 种子
 var seeds = []uint64{0xc3a5c85c97cb3127, 0xb492b66fbe98f273, 0x9ae16a3b2f90404f, 0xcbf29ce484222325}
 
-// 计数，类似于布隆过滤器，根据哈希映射到多个位置，然后在对应位置进行计数
+// 计数器，原理类似于布隆过滤器，根据哈希映射到多个位置，然后在对应位置进行计数
 // 读取时拿对应位置最小的
 type Counter struct {
 	counters []uint64
 	mask     uint64
 	// 每计数多少次应该减少计数
 	samples uint64
-	// Count()的次数
-	times uint64
+	// Add()的次数
+	additions uint64
 }
 
 func New(width uint64) *Counter {
@@ -42,42 +44,45 @@ func New(width uint64) *Counter {
 }
 
 // 根据增加对应位置的计数
-func (c *Counter) Count(hash uint64) {
-	start := (hash & 3) << 2
-
-	index0 := c.index(hash, 0)
-	index1 := c.index(hash, 1)
-	index2 := c.index(hash, 2)
-	index3 := c.index(hash, 3)
-
-	added := c.count(index0, start)
-	added = c.count(index1, start+1) || added
-	added = c.count(index2, start+2) || added
-	added = c.count(index3, start+3) || added
+func (c *Counter) Add(hash uint64) {
+	added := false
+	for depth := uint64(0); depth < counterDepth; depth++ {
+		index, offset := c.pos(hash, depth)
+		added = c.add(index, offset) || added
+	}
 
 	if added {
-		c.times++
-		if c.times == c.samples {
-			c.reset()
+		c.additions++
+		if c.additions == c.samples {
+			c.Reset()
 		}
 	}
 }
 
 // 估算对应hash的计数
 func (c *Counter) Estimate(hash uint64) uint64 {
-	start := (hash & 3) << 2
 	minCount := uint64(math.MaxUint64)
 	for depth := uint64(0); depth < counterDepth; depth++ {
-		index := c.index(hash, depth)
-		count := (c.counters[index] >> ((start + depth) << 2)) & 0xf
+		index, offset := c.pos(hash, depth)
+		count := (c.counters[index] >> offset) & countMask
 		minCount = mmath.Min(minCount, count)
 	}
 	return minCount
 }
 
-func (c *Counter) count(index, start uint64) bool {
-	offset := start << 2
-	mask := uint64(countBits*counterDepth-1) << offset
+// 计数减半
+func (c *Counter) Reset() {
+	for i, count := range c.counters {
+		if count != 0 {
+			c.counters[i] = (count >> 1) & 0x7777777777777777
+		}
+	}
+	c.additions = 0
+}
+
+// 增加对应下标和偏移的计数
+func (c *Counter) add(index, offset uint64) bool {
+	mask := countMask << offset
 	if c.counters[index]&mask != mask {
 		c.counters[index] += 1 << offset
 		return true
@@ -85,19 +90,13 @@ func (c *Counter) count(index, start uint64) bool {
 	return false
 }
 
-// 返回counters下标
-func (c *Counter) index(hash, depth uint64) uint64 {
+// 返回hash在counters的位置
+// index是数组下标
+// offset是对应元素的偏移
+func (c *Counter) pos(hash, depth uint64) (index uint64, offset uint64) {
 	hash = (hash + seeds[depth]) * seeds[depth]
 	hash += (hash >> 32)
-	return hash & c.mask
-}
-
-// 计数减半
-func (c *Counter) reset() {
-	for i, count := range c.counters {
-		if count != 0 {
-			c.counters[i] = (count >> 1) & 0x7777777777777777
-		}
-	}
-	c.times = 0
+	index = hash & c.mask
+	offset = ((hash&(counterDepth-1))*counterDepth + depth) * countBits
+	return
 }
--- counter/cm/cm_test.go
+++ counter/cm/cm_test.go
@@ -6,12 +6,12 @@ import (
 
 func TestCount(t *testing.T) {
 	cm := New(10)
-	cm.Count(10)
-	cm.Count(51151)
-	cm.Count(321)
-	cm.Count(10)
-	cm.Count(10)
-	cm.Count(321)
+	cm.Add(10)
+	cm.Add(51151)
+	cm.Add(321)
+	cm.Add(10)
+	cm.Add(10)
+	cm.Add(321)
 	if cm.Estimate(10) != 3 {
 		t.Errorf("want %v, but %d", 3, cm.Estimate(10))
 	}
@@ -23,19 +23,19 @@ func TestCount(t *testing.T) {
 	}
 
 	for i := 0; i < 100; i++ {
-		cm.Count(10)
+		cm.Add(10)
 	}
 	if cm.Estimate(10) != 15 {
 		t.Errorf("want %v, but %d", 15, cm.Estimate(10))
 	}
 	for i := 0; i < 100; i++ {
-		cm.Count(5)
+		cm.Add(5)
 	}
-	if cm.Estimate(10) != 15 {
-		t.Errorf("want %v, but %d", 15, cm.Estimate(10))
+	if cm.Estimate(10) != 7 {
+		t.Errorf("want %v, but %d", 7, cm.Estimate(10))
 	}
 	for i := 0; i < 100; i++ {
-		cm.Count(uint64(1))
+		cm.Add(uint64(1))
 	}
 	if cm.Estimate(10) != 7 {
 		t.Errorf("want %v, but %d", 7, cm.Estimate(10))
