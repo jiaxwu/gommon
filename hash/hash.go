2e3159624b88ce888ecdd9963d0d8ba36d01afd1
xhsf <827032783@qq.com>
2022-09-11 22:50:55 +0800

    hash

--- hash/hash.go
+++ hash/hash.go
@@ -7,12 +7,12 @@ import (
 // 哈希函数
 // 非线程安全，业务请加锁
 // 也就是对maphash的包装
-type Hasher struct {
+type Hash struct {
 	h *maphash.Hash
 }
 
-func New() *Hasher {
-	h := &Hasher{
+func New() *Hash {
+	h := &Hash{
 		h: &maphash.Hash{},
 	}
 	h.h.SetSeed(maphash.MakeSeed())
@@ -20,13 +20,13 @@ func New() *Hasher {
 }
 
 // 计算哈希值
-func (h *Hasher) Sum64(b []byte) uint64 {
+func (h *Hash) Sum64(b []byte) uint64 {
 	h.h.Reset()
 	h.h.Write(b)
 	return h.h.Sum64()
 }
 
 // 计算哈希值
-func (h *Hasher) Sum64String(s string) uint64 {
+func (h *Hash) Sum64String(s string) uint64 {
 	return h.Sum64([]byte(s))
 }
